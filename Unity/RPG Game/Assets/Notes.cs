478e65b37fa65c972d8787d1b377d35d3a9cb2b3
C6WX <163308814+C6WX@users.noreply.github.com>
2024-10-04 15:13:15 +0100

    Worked more on the journal and added buttons

---
+++ Unity/RPG Game/Assets/Notes.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+// make the game like an idle fighting game where the player rolls a dice to determine the damage they deal
+// the player and enemy will have health and damage
+// as the player kills enemies, they will gain xp and when they level up it will increase their stats
+}
